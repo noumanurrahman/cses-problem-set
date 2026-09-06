1570d33eaf5545295435c4f9f849c438d36bc1f2
noumanurrahman <hey@noumanrahman.com>
2026-09-07 00:55:24 +0530

    update: problem - tower of hanoi

--- introductory-problems/tower_of_hanoi.cpp
+++ introductory-problems/tower_of_hanoi.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void solve(int n, int a, int b) {
-  int free_pos = ((a == 1 && b == 2) || (a == 2 && b == 1))   ? 3
-                 : ((a == 1 && b == 3) || (a == 3 && b == 1)) ? 2
-                 : ((a == 2 && b == 3) || (a == 3 && b == 2)) ? 1
-                                                              : 0;
-
+void solve(int n, int a, int b, int c) {
   if (n == 1) {
     cout << 1 << " " << 3 << '\n';
   } else if (n == 2) {
-    cout << a << " " << free_pos << '\n';
+    cout << a << " " << c << '\n';
     cout << a << " " << b << '\n';
-    cout << free_pos << " " << b << '\n';
+    cout << c << " " << b << '\n';
   } else {
-    solve(n - 1, a, free_pos);
+    solve(n - 1, a, c, b);
     cout << a << " " << b << '\n';
-    solve(n - 1, free_pos, b);
+    solve(n - 1, c, b, a);
   }
 }
 
@@ -25,5 +20,5 @@ int main() {
   cin >> n;
   int k = (1 << n) - 1;
   cout << k << '\n';
-  solve(n, 1, 3);
+  solve(n, 1, 3, 2);
 }
