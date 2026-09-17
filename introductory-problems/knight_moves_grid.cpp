328926618d52c1cf250fd5c49889b353e3a22c67
noumanurrahman <hey@noumanrahman.com>
2026-09-18 01:08:42 +0530

    update: knight moves problem

--- introductory-problems/knight_moves_grid.cpp
+++ introductory-problems/knight_moves_grid.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 typedef vector<vector<int>> two_d;
+vector<pair<int, int>> moves = {{1, 2},  {2, 1},  {-1, 2},  {2, -1},
+                                {1, -2}, {-2, 1}, {-2, -1}, {-1, -2}};
 
-void moves(two_d &grid) {
+void solve(two_d &grid) {
   vector<pair<int, int>> queue;
   queue.push_back({0, 0});
   for (int q = 0; q < (int)queue.size(); q++) {
@@ -11,58 +13,15 @@ void moves(two_d &grid) {
     int j = queue[q].second;
     int initial = grid[i][j];
     int x, y;
-    vector<pair<int, int>> possible_moves;
-    if (i + 2 < (int)grid.size()) {
-      y = i + 2;
-      if (j + 1 < (int)grid.size() && grid[y][j + 1] == 0) {
-        x = j + 1;
-        possible_moves.push_back({y, x});
-      }
-      if (j - 1 >= 0 && grid[y][j - 1] == 0) {
-        x = j - 1;
-        possible_moves.push_back({y, x});
-      }
-    }
-    if (i - 2 >= 0) {
-      y = i - 2;
-      if (j + 1 < (int)grid.size() && grid[y][j + 1] == 0) {
-        x = j + 1;
-        possible_moves.push_back({y, x});
-      }
-      if (j - 1 >= 0 && grid[y][j - 1] == 0) {
-        x = j - 1;
-        possible_moves.push_back({y, x});
-      }
-    }
-    if (i + 1 < (int)grid.size()) {
-      y = i + 1;
-      if (j + 2 < (int)grid.size() && grid[y][j + 2] == 0) {
-        x = j + 2;
-        possible_moves.push_back({y, x});
-      }
-      if (j - 2 >= 0 && grid[y][j - 2] == 0) {
-        x = j - 2;
-        possible_moves.push_back({y, x});
-      }
-    }
-    if (i - 1 >= 0) {
-      y = i - 1;
-      if (j + 2 < (int)grid.size() && grid[y][j + 2] == 0) {
-        x = j + 2;
-        possible_moves.push_back({y, x});
-      }
-      if (j - 2 >= 0 && grid[y][j - 2] == 0) {
-        x = j - 2;
-        possible_moves.push_back({y, x});
-      }
-    }
-    for (auto move : possible_moves) {
-      y = move.first;
-      x = move.second;
+    for (auto move : moves) {
+      y = i + move.first;
+      x = j + move.second;
       if (x == 0 && y == 0)
         continue;
-      grid[y][x] = initial + 1;
-      queue.push_back({y, x});
+      if (min(x, y) >= 0 && max(x, y) < (int)grid.size() && grid[y][x] == 0) {
+        grid[y][x] = initial + 1;
+        queue.push_back({y, x});
+      }
     }
   }
 }
@@ -71,7 +30,7 @@ int main() {
   int n;
   cin >> n;
   two_d grid(n, vector<int>(n, 0));
-  moves(grid);
+  solve(grid);
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cout << grid[i][j] << ' ';
